3952034fdd61c8176b496d5b3db54147ced933a2
carlitosft <carlostttt2023@gmail.com>
2025-11-04 17:53:52 -0300

    nuevas cosas

--- DGSbd.sql
+++ DGSbd.sql
@@ -26,3 +26,24 @@ CREATE TABLE Usuario (
    
     fecha_fin DATETIME
  );
+
+CREATE TABLE Tablero(
+    id_tablero INT AUTO_INCREMENT PRIMARY KEY,
+    id_partida INT NOT NULL,
+    FOREIGN KEY (id_partida) REFERENCES Partida(id_partida)
+);
+
+CREATE TABLE Recinto(
+    id_recinto INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    id_tablero INT NOT NULL,
+    FOREIGN KEY (id_tablero) REFERENCES Tablero(id_tablero)
+);
+
+CREATE TABLE Juega(
+    id_usuario INT NOT NULL,
+    id_partida INT NOT NULL,
+    PRIMARY KEY (id_usuario, id_partida),
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
+    FOREIGN KEY (id_partida) REFERENCES Partida(id_partida)
+);
